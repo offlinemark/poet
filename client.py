8106f1929eec59fff1747b27715fc59536b5deef
Mark Mossberg <mark.mossberg@gmail.com>
2015-02-14 22:27:43 -0500

    Clean up
    Tested on OS X, works. Clean up some comments that aren't relevant
    anymore.

--- client.py
+++ client.py
@@ -191,17 +191,12 @@ class PoetClient(object):
         """
 
         r = urllib2.urlopen(inp.split()[1])
-
-        # don't delete the file automatically because otherwise something weird
-        # happens and we might (will) delete the file before it gets loaded
-        # into memory? if we didn't include this, we would need to call wait()
-        # or sp.call() instead later on
         with tempfile.NamedTemporaryFile(delete=False) as f:
             f.write(r.read())
             os.fchmod(f.fileno(), stat.S_IRWXU)
-            # f.flush()  # ensure that file was actually written to disk
-            # intentionally not using sp.call() here because we don't
-            # necessarily want to wait() on the process
+        # intentionally not using sp.call() here because we don't
+        # necessarily want to wait() on the process. also, this is outside
+        # the with block to avoid a `Text file busy' error (linux)
         sp.Popen(f.name, stdout=open(os.devnull, 'w'), stderr=sp.STDOUT)
 
     def chint(self, s, inp):
