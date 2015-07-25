c4592bd8cbbc055ba52f098a507e1da1156d1241
Mark Mossberg <mark.mossberg@gmail.com>
2015-07-25 02:07:37 -0400

    Add template module

---
+++ common/modules/template.py
@@ -0,0 +1,50 @@
+# template, example module
+
+#
+# required
+#
+import module
+
+#
+# your imports
+#
+
+# import blah
+
+#
+# global vars
+#
+
+MODNAME = 'template'
+USAGE = """
+"""
+
+#
+# handlers
+#
+
+
+@module.server_handler(MODNAME)
+def server(server, argv):
+    """Server handler for module.
+
+    Args:
+        server: instance of PoetServer class
+        argv: list of arguments entered at server shell
+    """
+
+    # your code goes here
+    pass
+
+
+@module.client_handler(MODNAME)
+def client(client, inp):
+    """Client handler for module.
+
+    Args:
+        client: instance of PoetClient class
+        inp: command string sent from server
+    """
+
+    # your code goes here
+    pass
